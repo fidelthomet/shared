0e21c3dc323151088c3e83fa779ed1fb9cbbf3b6
Fidel Thomet <me@fidelthomet.com>
2018-06-07 11:23:08 +0200

    [Update] added linechart component

---
+++ components/VisLineChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    width="400"
+    height="300">
+    <polyline points="0,0 400,300"/>
+    <polyline points="0,300 400,0"/>
+  </svg>
+</template>
+
+<style>
+polyline {
+  stroke: #000;
+  fill: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,17 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        shared
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <VisLineChart/>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import VisLineChart from '~/components/VisLineChart.vue'
 
 export default {
   components: {
-    AppLogo
+    VisLineChart
   }
 }
 </script>
